256fec3ac45bc74d915032a92ec7b176ac0fe889
Shilpy <79649004+shili-51@users.noreply.github.com>
2026-07-16 19:04:29 +0530

    Update Violation fixed.java

--- Open-Close Principle/Violation fixed.java
+++ Open-Close Principle/Violation fixed.java
@@ -1,45 +1,40 @@
-interface paymentMethod{
+interface PaymentMethod {
     void pay();
 }
 
-
-class paymentProcessor{
-    public void payment(paymentMethod payment){
-        payment.pay();
+class PaymentProcessor {
+    public void processPayment(PaymentMethod paymentMethod) {
+        paymentMethod.pay();
     }
 }
 
-class Cash implements paymentMethod{
-    public void pay(){
+class Cash implements PaymentMethod {
+    @Override
+    public void pay() {
         System.out.println("Paid via Cash");
     }
 }
 
-class UPI implements paymentMethod{
-    public void pay(){
+class UPI implements PaymentMethod {
+    @Override
+    public void pay() {
         System.out.println("Paid via UPI");
     }
 }
 
-class Netbanking implements paymentMethod{
-    public void pay(){
-        System.out.println("Paid via Netbanking");
+class NetBanking implements PaymentMethod {
+    @Override
+    public void pay() {
+        System.out.println("Paid via NetBanking");
     }
 }
 
-// newfeature 
-class debitCard implements paymentMethod{
-    public void pay(){
-        System.out.println("Paid via debitCard");
-    }
-}
+public class Main {
+    public static void main(String[] args) {
+        PaymentProcessor processor = new PaymentProcessor();
 
-
-
-public class Main{
-    public static void main(String[] args){
-        paymentProcessor processor = new paymentProcessor();
-        // processor.payment(new Cash());
-        processor.payment(new debitCard());
+        processor.processPayment(new Cash());
+        processor.processPayment(new UPI());
+        processor.processPayment(new NetBanking());
     }
 }
